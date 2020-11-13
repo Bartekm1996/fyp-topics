ee64de6a0e549c465d9163ea7a2c23d51ec39f35
Bartlomiej Mlynarkiewicz <bmlynarkiewicz@hotmail.com>
2020-11-13 12:05:10 +0000

    Changed layout of footer

--- presets/argon/src/argon-stubs/resources/views/users/index.blade.php
+++ presets/argon/src/argon-stubs/resources/views/users/index.blade.php
@@ -186,11 +186,7 @@
                   <span class="nav-link-text">Tables</span>
                 </a>
             </li>
-                           <li class="nav-item mb-5 mr-4 ml-4 pl-1 bg-danger" style="position: absolute; bottom: 0;">
-                    <a class="nav-link text-white" href="https://www.creative-tim.com/product/argon-dashboard-pro-laravel" target="_blank">
-                        <i class="ni ni-cloud-download-95"></i> Upgrade to PRO
-                    </a>
-                </li>
+
         </ul>
         <!-- Divider -->
         <hr class="my-3">
@@ -431,35 +427,7 @@
             </div>
         </div>
     </div>
-        
-    <footer class="footer">
-<div class="row align-items-center justify-content-xl-between">
-<div class="col-xl-6">
-    <div class="copyright text-center text-xl-left text-muted">
-        © 2020 <a href="https://www.creative-tim.com" class="font-weight-bold ml-1" target="_blank">Creative Tim</a> &amp;
-        <a href="https://www.updivision.com" class="font-weight-bold ml-1" target="_blank">Updivision</a>
     </div>
-</div>
-<div class="col-xl-6">
-    <ul class="nav nav-footer justify-content-center justify-content-xl-end">
-        <li class="nav-item">
-            <a href="https://www.creative-tim.com" class="nav-link" target="_blank">Creative Tim</a>
-        </li>
-        <li class="nav-item">
-            <a href="https://www.updivision.com" class="nav-link" target="_blank">Updivision</a>
-        </li>
-        <li class="nav-item">
-            <a href="https://www.creative-tim.com/presentation" class="nav-link" target="_blank">About Us</a>
-        </li>
-        <li class="nav-item">
-            <a href="http://blog.creative-tim.com" class="nav-link" target="_blank">Blog</a>
-        </li>
-        <li class="nav-item">
-            <a href="https://github.com/creativetimofficial/argon-dashboard/blob/master/LICENSE.md" class="nav-link" target="_blank">MIT License</a>
-        </li>
-    </ul>
-</div>
-</div></footer>    </div>
     </div>
 
     
