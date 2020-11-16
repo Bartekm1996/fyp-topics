55f50ad0ef2f2683ad0e5fdf58c1d1553baec822
Paul Kinsella <paulkinselladev@gmail.com>
2020-11-16 11:30:31 +0000

    Update Logging. Show message count for each type

--- resources/views/logging/index.blade.php
+++ resources/views/logging/index.blade.php
@@ -275,8 +275,8 @@
                     <div class="card-body">
                         <div class="row">
                             <div class="col">
-                                <h5 class="card-title text-uppercase text-muted mb-0">Traffic</h5>
-                                <span class="h2 font-weight-bold mb-0">350,897</span>
+                                <h5 class="card-title text-uppercase text-muted mb-0">Total Messages</h5>
+                                <span class="h2 font-weight-bold mb-0">{{count($messages)}}</span>
                             </div>
                             <div class="col-auto">
                                 <div class="icon icon-shape bg-danger text-white rounded-circle shadow">
@@ -296,12 +296,12 @@
                     <div class="card-body">
                         <div class="row">
                             <div class="col">
-                                <h5 class="card-title text-uppercase text-muted mb-0">New users</h5>
-                                <span class="h2 font-weight-bold mb-0">2,356</span>
+                                <h5 class="card-title text-uppercase text-muted mb-0">Debug</h5>
+                                <span class="h2 font-weight-bold mb-0">{{count($messages->where('type','=',0))}}</span>
                             </div>
                             <div class="col-auto">
-                                <div class="icon icon-shape bg-warning text-white rounded-circle shadow">
-                                    <i class="fas fa-chart-pie"></i>
+                                <div class="icon icon-shape bg-primary text-white rounded-circle shadow">
+                                    <i class="fas fa-bug"></i>
                                 </div>
                             </div>
                         </div>
@@ -317,12 +317,12 @@
                     <div class="card-body">
                         <div class="row">
                             <div class="col">
-                                <h5 class="card-title text-uppercase text-muted mb-0">Sales</h5>
-                                <span class="h2 font-weight-bold mb-0">924</span>
+                                <h5 class="card-title text-uppercase text-muted mb-0">Info</h5>
+                                <span class="h2 font-weight-bold mb-0">{{count($messages->where('type','=',1))}}</span>
                             </div>
                             <div class="col-auto">
-                                <div class="icon icon-shape bg-yellow text-white rounded-circle shadow">
-                                    <i class="fas fa-users"></i>
+                                <div class="icon icon-shape bg-gradient-gray text-white rounded-circle shadow">
+                                    <i class="fas fa-info-circle"></i>
                                 </div>
                             </div>
                         </div>
@@ -338,12 +338,12 @@
                     <div class="card-body">
                         <div class="row">
                             <div class="col">
-                                <h5 class="card-title text-uppercase text-muted mb-0">Performance</h5>
-                                <span class="h2 font-weight-bold mb-0">49,65%</span>
+                                <h5 class="card-title text-uppercase text-muted mb-0">Error</h5>
+                                <span class="h2 font-weight-bold mb-0">{{count($messages->where('type','=',2))}}</span>
                             </div>
                             <div class="col-auto">
-                                <div class="icon icon-shape bg-info text-white rounded-circle shadow">
-                                    <i class="fas fa-percent"></i>
+                                <div class="icon icon-shape bg-red text-white rounded-circle shadow">
+                                    <i class="fas fa-exclamation-triangle"></i>
                                 </div>
                             </div>
                         </div>
