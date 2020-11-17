09c9d3deda813d92526e917660cbabbcb280c538
Paul Kinsella <paulkinselladev@gmail.com>
2020-11-17 23:17:59 +0000

    Add disable update email if user is a student. Their student id is taken from this so not good idea to change

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -107,7 +107,7 @@
                                 </div>
                                 <div class="form-group{{ $errors->has('email') ? ' has-danger' : '' }}">
                                     <label class="form-control-label" for="input-email">{{ __('Email') }}</label>
-                                    <input type="email" name="email" id="input-email" class="form-control form-control-alternative{{ $errors->has('email') ? ' is-invalid' : '' }}" placeholder="{{ __('Email') }}" value="{{ old('email', auth()->user()->email) }}" required>
+                                    <input @if(auth()->user()->role == 0)disabled @endif type="email" name="email" id="input-email" class="form-control form-control-alternative{{ $errors->has('email') ? ' is-invalid' : '' }}" placeholder="{{ __('Email') }}" value="{{ old('email', auth()->user()->email) }}" required>
 
                                     @if ($errors->has('email'))
                                         <span class="invalid-feedback" role="alert">
