ffaa08627c21afba678052942a5f7397b1e8bda2
nc <n3nmail@gmail.com>
2020-11-18 13:08:51 +0000

    Resize UL logo - test

--- resources/views/layouts/navbars/navs/guest.blade.php
+++ resources/views/layouts/navbars/navs/guest.blade.php
@@ -1,7 +1,7 @@
 <nav class="navbar navbar-top navbar-horizontal navbar-expand-md navbar-dark">
-    <div class="container px-4">
+    <div class="container px-3">
         <a class="navbar-brand" href="{{ route('home') }}">
-            <img src="{{ asset('argon') }}/img/brand/ul.png" width="350" height="350"/>
+            <img src="{{ asset('argon') }}/img/brand/ul.png" width="120" height="32" />
         </a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar-collapse-main" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
