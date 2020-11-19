62854b2d400c127556e420d1de11233a2624668e
Paul Kinsella <paulkinselladev@gmail.com>
2020-11-19 17:59:42 +0000

    Change input to button, use font-awesome icons

--- app/Models/FypEventState.php
+++ app/Models/FypEventState.php
@@ -7,6 +7,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class FypEventState extends Model
 {
+    const IDLE = 0;
+    const IN_PROGRESS = 1;
+    const COMPLETE = 2;
+
     use HasFactory;
     protected $fillable = [
         'started_on',
