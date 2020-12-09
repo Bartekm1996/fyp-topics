9e43aadccdd842626bd43c621d05cc282b56dd33
Paul Kinsella <paulkinselladev@gmail.com>
2020-12-09 11:02:27 +0000

    Reroute to topics after login

--- routes/web.php
+++ routes/web.php
@@ -19,7 +19,7 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', 'App\Http\Controllers\HomeController@index')->name('home');
+Route::get('/home', 'App\Http\Controllers\TopicsController@index')->name('home');
 
 Route::group(['middleware' => 'auth'], function () {
 	Route::resource('user', 'App\Http\Controllers\UserController', ['except' => ['show']]);
