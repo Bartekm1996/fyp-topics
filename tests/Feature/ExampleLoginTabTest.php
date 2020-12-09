e481dddf7e0386fd6a3d361737c578e2f0e7fe01
nc <n3nmail@gmail.com>
2020-12-09 13:29:31 +0000

    Example login tab test

--- tests/Feature/ExampleLoginTabTest.php
+++ tests/Feature/ExampleLoginTabTest.php
@@ -5,7 +5,7 @@ namespace Tests\Feature;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
-class ExampleTest extends TestCase
+class ExampleLoginTabTest extends TestCase
 {
     /**
      * A basic test example.
@@ -15,7 +15,7 @@ class ExampleTest extends TestCase
     public function testBasicTest()
     {
         $response = $this->get('/');
-
+        $response = $this->get('/login');
         $response->assertStatus(200);
     }
 }
